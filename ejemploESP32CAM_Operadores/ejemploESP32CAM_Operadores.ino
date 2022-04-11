ec540122a715c8594074850a7119f45325155dad
armando-cruz-cruz <armando.cruz@uvmnet.edu>
2022-04-11 09:47:08 -0500

    Update ejemploESP32CAM_Operadores.ino

--- ejemploESP32CAM_Operadores/ejemploESP32CAM_Operadores.ino
+++ ejemploESP32CAM_Operadores/ejemploESP32CAM_Operadores.ino
@@ -1,13 +1,14 @@
 /*
- * Estructura básica
- * Por: Hugo Vargas
- * Fecha: 28 de junio de 2021
+ * Ejemplo Modulo
  * 
- * Esto es una muestra de la estructura básica de un programa
+ * Por: Armando Cruz
+ * Fecha: 11 de abril de 2021
+ * 
+ * Este programa tiene como proposito ejemplificar el uso del operador modulo
  */
 
 // Bibliotecas
-//#include<Serial.h>
+
 // Constantes
 
 // Variables
@@ -18,17 +19,22 @@ int dato=0;
 // Condiciones iniciales - Se ejecuta sólo una vez al energizar
 void setup() {// Inicio de void setup ()
   // Aquí va tu código
-  Serial.begin(115200);
-  Serial.println("Conexion iniciada..");
+  Serial.begin(115200); //Iniciar comunicacion serial
+  Serial.println("Se ha iniciado la comunicacion serial..");
 }// Fin de void setup
 
 // Cuerpo del programa - Se ejecuta constamente
 void loop() {// Inicio de void loop
   // put your main code here, to run repeatedly:
-  dato++;
+  dato++; // dato=dato+1; forma alternativa
   Serial.println(dato);
   delay(1000);
-  dato%=8;
+  //Contador de anillo con comparaciones
+  if (dato>8) {
+    dato=0;
+  }
+  //Contador de anillo con operador
+  //dato%=8;
 }// Fin de void loop
 
 // Funcioes del usuario
